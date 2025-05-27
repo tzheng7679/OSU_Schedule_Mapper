bd2f8be86eb149bffa83a4a35361a1d99302df24
trollol7679 <69732325+trollol7679@users.noreply.github.com>
2025-05-27 15:14:24 -0400

    stylized page, fixed wrong "your" (shame)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, QueryList, ViewChildren } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { GetLocsComponent } from "./get-locs/get-locs.component";
 import { Course, DAYS } from './get-locs/sched_parser';
 import { MapTabComponent } from "./map-tab/map-tab.component";
 
 @Component({
 	selector: 'app-root',
-	imports: [MapTabComponent, GetLocsComponent],
+	imports: [MapTabComponent, GetLocsComponent, NgOptimizedImage],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css'
 })
